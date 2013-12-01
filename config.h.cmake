68b216f4ead5d5e62a7daa6198a33e773720d9a8
Vladimir S. FONOV <vladimir.fonov@gmail.com>
2013-11-30 20:22:24 -0500

    Added missing config.h.cmake

---
+++ config.h.cmake
@@ -0,0 +1,141 @@
+/* Define to 1 if you have the <dlfcn.h> header file. */
+#cmakedefine HAVE_DLFCN_H 1
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#cmakedefine HAVE_INTTYPES_H 1
+
+/* Define to 1 if you have the <memory.h> header file. */
+#cmakedefine HAVE_MEMORY_H 1
+
+/* Define if you have POSIX threads libraries and header files. */
+#cmakedefine HAVE_PTHREAD 1
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#cmakedefine HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#cmakedefine HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#cmakedefine HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#cmakedefine HAVE_STRING_H 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#cmakedefine HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#cmakedefine HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#cmakedefine HAVE_UNISTD_H 1
+
+/* Define to necessary symbol if this constant uses a non-standard name on
+   your system. */
+#cmakedefine PTHREAD_CREATE_JOINABLE
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+
+/* Define if the X Window System is missing or not being used.  */
+#cmakedefine X_DISPLAY_MISSING
+
+/* Define if you have alloca, as a function or macro.  */
+#cmakedefine HAVE_ALLOCA
+
+/* Define if you have <alloca.h> and it should be used (not on Ultrix).  */
+#cmakedefine HAVE_ALLOCA_H
+
+/* Define if your system has a working fnmatch function.  */
+#cmakedefine HAVE_FNMATCH
+
+/* Define if you have <sys/wait.h> that is POSIX.1 compatible.  */
+#cmakedefine HAVE_SYS_WAIT_H
+
+/* Define as __inline if that's what the C compiler calls it.  */
+#cmakedefine inline
+
+
+/* image libs */
+#cmakedefine HAVE_LIBJPEG
+#cmakedefine HAVE_LIBTIFF
+#cmakedefine HAVE_LIBPNG
+
+/* X shm extention */
+#cmakedefine HAVE_XSHM
+#cmakedefine HAVE_X_SHAPE_EXT
+
+
+/* Define if you have the getcwd function.  */
+#cmakedefine HAVE_GETCWD
+
+/* Define if you have the gethostname function.  */
+#cmakedefine HAVE_GETHOSTNAME
+
+/* Define if you have the gettimeofday function.  */
+#cmakedefine HAVE_GETTIMEOFDAY
+
+/* Define if you have the select function.  */
+#cmakedefine HAVE_SELECT
+
+/* Define if you have the strtol function.  */
+#cmakedefine HAVE_STRTOL
+
+/* Define if you have the <dirent.h> header file.  */
+#cmakedefine HAVE_DIRENT_H
+
+/* Define if you have the <fcntl.h> header file.  */
+#cmakedefine HAVE_FCNTL_H
+
+/* Define if you have the <limits.h> header file.  */
+#cmakedefine HAVE_LIMITS_H
+
+/* Define if you have the <malloc.h> header file.  */
+#cmakedefine HAVE_MALLOC_H
+
+/* Define if you have the <ndir.h> header file.  */
+#cmakedefine HAVE_NDIR_H
+
+/* Define if you have the <sgtty.h> header file.  */
+#cmakedefine HAVE_SGTTY_H
+
+/* Define if you have the <stdarg.h> header file.  */
+#cmakedefine HAVE_STDARG_H
+
+/* Define if you have the <stdlib.h> header file.  */
+#cmakedefine HAVE_STDLIB_H
+
+/* Define if you have the <string.h> header file.  */
+#cmakedefine HAVE_STRING_H
+
+/* Define if you have the <strings.h> header file.  */
+#cmakedefine HAVE_STRINGS_H
+
+/* Define if you have the <sys/dir.h> header file.  */
+#cmakedefine HAVE_SYS_DIR_H
+
+/* Define if you have the <sys/ioctl.h> header file.  */
+#cmakedefine HAVE_SYS_IOCTL_H
+
+/* Define if you have the <sys/ndir.h> header file.  */
+#cmakedefine HAVE_SYS_NDIR_H
+
+/* Define if you have the <sys/select.h> header file.  */
+#cmakedefine HAVE_SYS_SELECT_H
+
+/* Define if you have the <sys/time.h> header file.  */
+#cmakedefine HAVE_SYS_TIME_H
+
+/* Define if you have the <termios.h> header file.  */
+#cmakedefine HAVE_TERMIOS_H
+
+
+#define PACKAGE "@PACKAGE_NAME@"
+#define PACKAGE_NAME "@PACKAGE_NAME@"
+#define PACKAGE_BUGREPORT "@PACKAGE_BUGREPORT@"
+#define PACKAGE_VERSION "@PACKAGE_VERSION@"
+#define PACKAGE_STRING "@PACKAGE_STRING@"
+#define VERSION PACKAGE_VERSION
+
